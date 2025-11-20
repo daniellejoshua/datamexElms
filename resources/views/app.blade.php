4e40e6afff8c95636501b2b20598646cffa62df6
Danielle Joshua Esternon <daniellejoshua18@gmail.com>
2025-11-20 20:18:02 +0800

    Remove other controller rolling back from fresh start of react and laravel

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,23 +1,44 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<!DOCTYPE html><!DOCTYPE html>
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}"><html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <head>    <head>
 
-        <!-- Scripts -->
-        @routes
-        @viteReactRefresh
-        @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
-        @inertiaHead
-    </head>
-    <body class="font-sans antialiased">
-        @inertia
-    </body>
-</html>
+        <meta charset="utf-8">        <meta charset="utf-8">
+
+        <meta name="viewport" content="width=device-width, initial-scale=1">        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+
+        <title inertia>{{ config('app.name', 'Laravel') }}</title>        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+
+
+
+        <!-- Fonts -->        <!-- Fonts -->
+
+        <link rel="preconnect" href="https://fonts.bunny.net">        <link rel="preconnect" href="https://fonts.bunny.net">
+
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+
+
+        <!-- Scripts -->        <!-- Scripts -->
+
+        @routes        @routes
+
+        @viteReactRefresh        @viteReactRefresh
+
+        @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])        @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
+
+        @inertiaHead        @inertiaHead
+
+    </head>    </head>
+
+    <body class="font-sans antialiased">    <body class="font-sans antialiased">
+
+        @inertia        @inertia
+
+    </body>    </body>
+
+</html></html>
   
